52da82bba290ba850a1f35eb25fbcea40c66e4f1
vignesh sekar <vignesh1271@gmail.com>
2024-09-11 10:42:55 +0530

    .DS_Store banished!

---
+++ Section 10 - Google Cloud SQL/create_table_titanic.sql
@@ -0,0 +1,16 @@
+drop table if exists titanic;
+
+CREATE TABLE titanic (
+  "PassengerId" TEXT,
+  "Survived" TEXT,
+  "Pclass" TEXT,
+  "Sex" TEXT,
+  "Age" TEXT,
+  "SibSp" TEXT,
+  "Parch" TEXT,
+  "Ticket" TEXT,
+  "Fare" TEXT,
+  "Cabin" TEXT,
+  "Embarked" TEXT
+  
+);
